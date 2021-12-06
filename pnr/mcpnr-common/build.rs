c835dcc657998c75295c0cc12a191a12296ddc6e
bobtwinkles <srkoser+github@gmail.com>
2021-12-05 23:23:53 -0800

    Revert "pnr-common: don't vendor yosys proto file"
    This reverts commit 01e6169c93776f460f3d24fde464d54fe38c9dae.

--- pnr/mcpnr-common/build.rs
+++ pnr/mcpnr-common/build.rs
@@ -1,29 +1,36 @@
 use std::env;
+use std::fs;
 use std::path;
 
 fn main() -> Result<(), std::io::Error> {
+    let out_dir = path::PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_dir = out_dir.join("protos");
+    let proto_cache_dir = path::PathBuf::from(&out_dir).join("cache");
+
+    fs::create_dir_all(&proto_cache_dir).expect("Failed to create proto file cache directory");
+
     let yosys_proto = {
-        let yosys_proto = path::PathBuf::from(
-            env::var_os("YOSYS_PROTO_PATH")
-                .expect("YOSYS_PROTO_PATH must be set to the path to the Yosys Protobuf spec file"),
-        );
-
+        let yosys_proto = env::var("YOSYS_PROTO_PATH")
+            .expect("YOSYS_PROTO_PATH must be set to the path to the Yosys Protobuf spec file");
         println!("cargo:rerun-if-env-changed=YOSYS_PROTO_PATH");
-        println!("cargo:rerun-if-changed={}", yosys_proto.to_string_lossy());
+        println!("cargo:rerun-if-changed={}", yosys_proto);
 
-        yosys_proto
+        let yosys_proto_cached = proto_cache_dir.join("yosys.proto");
+
+        if yosys_proto_cached.exists() {
+            fs::remove_file(&yosys_proto_cached).expect("Could not remove stale yosys proto");
+        }
+
+        fs::copy(&yosys_proto, &yosys_proto_cached).expect(&format!(
+            "Failed to copy {:?} to {:?}",
+            yosys_proto, yosys_proto_cached
+        ));
+
+        yosys_proto_cached
     };
-
-    let yosys_proto_dir = yosys_proto
-        .parent()
-        .expect(&format!(
-            "Failed to get parent directory of yosys protobuf file {}",
-            yosys_proto.to_string_lossy()
-        ))
-        .to_owned();
 
     prost_build::Config::new()
         .include_file("protos.rs")
-        .compile_protos(&[yosys_proto], &[yosys_proto_dir])?;
+        .compile_protos(&[yosys_proto], &[proto_cache_dir])?;
     Ok(())
 }
