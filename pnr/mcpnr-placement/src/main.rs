a05ab983c66da48b63aea040d1e4b08c36521843
bobtwinkles <srkoser+github@gmail.com>
2021-12-05 19:54:40 -0800

    placement: argument parsing

--- pnr/mcpnr-placement/src/main.rs
+++ pnr/mcpnr-placement/src/main.rs
@@ -1,5 +1,40 @@
 mod protos;
+use std::path::PathBuf;
+
+#[derive(Clone,Debug)]
+struct Config {
+    input_file: PathBuf,
+    output_file: PathBuf,
+}
+
+fn parse_args() -> Config {
+    use clap::{App, Arg};
+    let matches = App::new("MCPNR Placer")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(clap::crate_authors!())
+        .about("Placement phase for the MCPNR flow")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input design, as the output of a Yosys write_protobuf command")
+                .index(1)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Output file location")
+                .index(2)
+                .required(true),
+        )
+        .get_matches();
+
+    Config {
+        input_file: PathBuf::from(matches.value_of_os("INPUT").unwrap()),
+        output_file: PathBuf::from(matches.value_of_os("OUTPUT").unwrap()),
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let config = parse_args();
+
+    std::fs::copy(config.input_file, config.output_file);
 }
