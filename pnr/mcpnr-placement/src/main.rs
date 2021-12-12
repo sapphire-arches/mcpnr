61d77e4d5766142fb96891e2a11d17bbbf071b8e
bobtwinkles <srkoser+github@gmail.com>
2021-12-12 02:35:00 -0800

    placment: the world's worst placement algorithm
    Lays out every input cell in a grid. Wow!

--- pnr/mcpnr-placement/src/main.rs
+++ pnr/mcpnr-placement/src/main.rs
@@ -1,16 +1,21 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use itertools::Itertools;
 use mcpnr_common::prost::Message;
 use mcpnr_common::protos::mcpnr::{PlacedDesign, Position};
 use mcpnr_common::protos::yosys::pb::parameter::Value as YPValue;
 use mcpnr_common::protos::yosys::pb::{Design, Parameter};
 use mcpnr_common::protos::CellExt;
+use placement_cell::{CellFactory, PlacementCell};
+use std::collections::HashMap;
 use std::path::PathBuf;
+
+mod placement_cell;
 
 #[derive(Clone, Debug)]
 struct Config {
     input_file: PathBuf,
     output_file: PathBuf,
+    structure_directory: PathBuf,
 }
 
 fn parse_args() -> Config {
@@ -19,6 +24,12 @@ fn parse_args() -> Config {
         .version(env!("CARGO_PKG_VERSION"))
         .author(clap::crate_authors!())
         .about("Placement phase for the MCPNR flow")
+        .arg(
+            Arg::with_name("TECHLIB")
+                .long("techlib")
+                .value_name("TECHLIB")
+                .required(true),
+        )
         .arg(
             Arg::with_name("INPUT")
                 .help("Input design, as the output of a Yosys write_protobuf command")
@@ -33,14 +44,17 @@ fn parse_args() -> Config {
         )
         .get_matches();
 
+    let techlib_directory = PathBuf::from(matches.value_of_os("TECHLIB").unwrap());
+
     Config {
         input_file: PathBuf::from(matches.value_of_os("INPUT").unwrap()),
         output_file: PathBuf::from(matches.value_of_os("OUTPUT").unwrap()),
+        structure_directory: techlib_directory.join("structures"),
     }
 }
 
-fn place(design: Design) -> Result<PlacedDesign> {
-    let cells = design
+fn place(config: &Config, design: Design) -> Result<PlacedDesign> {
+    let top_module = design
         .modules
         .into_values()
         .find(|m| {
@@ -49,44 +63,66 @@ fn place(design: Design) -> Result<PlacedDesign> {
                     value: Some(YPValue::Int(1)),
                 })
         })
-        .unwrap()
+        .ok_or_else(|| anyhow!("Failed to locate top module"))?;
+
+    let mut cell_factory = CellFactory::new(config.structure_directory.clone());
+
+    let mut cells: HashMap<String, (PlacementCell, _)> = top_module
         .cell
         .into_iter()
-        .map(|(_, cell)| -> Result<_> {
-            fn get_pos_attrs(
-                cell: &mcpnr_common::protos::yosys::pb::module::Cell,
-            ) -> Result<(u32, u32, u32)> {
-                Ok((
-                    cell.get_param_i64_with_default("POS_X", 0)
-                        .map_err(anyhow::Error::from)
-                        .and_then(|v| v.try_into().map_err(anyhow::Error::from))
-                        .with_context(|| "Read attr POS_X")?,
-                    cell.get_param_i64_with_default("POS_Y", 0)
-                        .map_err(anyhow::Error::from)
-                        .and_then(|v| v.try_into().map_err(anyhow::Error::from))
-                        .with_context(|| "Read attr POS_Y")?,
-                    cell.get_param_i64_with_default("POS_Z", 0)
-                        .map_err(anyhow::Error::from)
-                        .and_then(|v| v.try_into().map_err(anyhow::Error::from))
-                        .with_context(|| "Read attr POS_Z")?,
-                ))
-            }
-
-            let (x, y, z) = match cell.r#type.as_ref() {
-                "MCPNR_SWITCHES" | "MCPNR_LIGHTS" => get_pos_attrs(&cell)
-                    .with_context(|| format!("Getting position from cell {:?}", &cell))?,
-                _ => (10, 0, 0),
-            };
-            let mcpnr_cell = mcpnr_common::protos::mcpnr::placed_design::Cell {
-                attribute: cell.attribute,
-                connection: cell.connection,
-                parameter: cell.parameter,
-                pos: Some(Position { x, y, z }),
-                r#type: cell.r#type,
-            };
-            Ok(mcpnr_cell)
+        .map(|(key, cell)| -> Result<_> {
+            Ok((
+                key,
+                (
+                    cell_factory.build_cell(&cell)?,
+                    (cell.attribute, cell.connection, cell.parameter, cell.r#type),
+                ),
+            ))
         })
         .try_collect()?;
+
+    // TODO: smart place
+    let mut cx = 0;
+    let mut cz = 5;
+    let mut row_max_z = 0;
+
+    for (_, (cell, _)) in cells.iter_mut() {
+        if cell.pos_locked {
+            continue;
+        }
+        if cx + cell.sx > 50 {
+            // TODO: don't hard code region size
+            cz += row_max_z;
+            row_max_z = 0;
+            cx = 0;
+        }
+        cell.x = cx;
+        cell.z = cz;
+
+        cx += cell.sx;
+        row_max_z = std::cmp::max(cell.sz, row_max_z);
+    }
+
+    // Convert intermediate placement cells to output format
+    let cells = cells
+        .into_iter()
+        .map(|(_, (cell, (attribute, connection, parameter, ty)))| {
+            let pos = cell.unexpanded_pos();
+            let mcpnr_cell = mcpnr_common::protos::mcpnr::placed_design::Cell {
+                attribute,
+                connection,
+                parameter,
+                pos: Some(Position {
+                    x: pos[0],
+                    y: pos[1],
+                    z: pos[2],
+                }),
+                r#type: ty,
+            };
+            mcpnr_cell
+        })
+        .collect();
+
     Ok(PlacedDesign {
         creator: format!(
             "Placed by MCPNR {}, Synth: {}",
@@ -101,11 +137,11 @@ fn main() -> Result<()> {
     let config = parse_args();
 
     let design = {
-        let inf = std::fs::read(config.input_file).unwrap();
+        let inf = std::fs::read(&config.input_file).unwrap();
         Design::decode(&inf[..]).unwrap()
     };
 
-    let placed_design = place(design)?;
+    let placed_design = place(&config, design)?;
 
     {
         use std::io::Write;
---
+++ pnr/mcpnr-placement/src/placement_cell.rs
@@ -0,0 +1,166 @@
+use anyhow::{Context, Result};
+use mcpnr_common::{minecraft_types::Structure, protos::yosys::pb::module::Cell, protos::CellExt};
+use std::{collections::HashMap, path::PathBuf};
+
+pub const XZ_EXPANSION: u32 = 2;
+
+struct PlacementStructureData {
+    sx: u32,
+    sy: u32,
+    sz: u32,
+}
+
+/// yeah it's a java thing get over it.
+///
+/// caches structure reads so we can avoid re-parsing on every cell
+pub struct CellFactory {
+    structure_directory: PathBuf,
+    structure_cache: HashMap<String, PlacementStructureData>,
+}
+
+pub struct PlacementCell {
+    pub x: u32,
+    pub y: u32,
+    pub z: u32,
+    pub sx: u32,
+    pub sy: u32,
+    pub sz: u32,
+    pub pos_locked: bool,
+}
+
+impl PlacementCell {
+    pub fn unexpanded_pos(&self) -> [u32; 3] {
+        if self.pos_locked {
+            [self.x, self.y, self.z]
+        } else {
+            [self.x + XZ_EXPANSION, self.y, self.z + XZ_EXPANSION]
+        }
+    }
+}
+
+impl CellFactory {
+    pub fn new(structure_directory: PathBuf) -> Self {
+        Self {
+            structure_directory,
+            structure_cache: Default::default(),
+        }
+    }
+
+    pub fn load_structure(&mut self, structure_name: &str) -> Result<&PlacementStructureData> {
+        if self.structure_cache.contains_key(structure_name) {
+            self.structure_cache
+                .get(structure_name)
+                .ok_or_else(|| -> ! { unreachable!() })
+                .map_err(Into::into)
+        } else {
+            let nbt_cell_file = self.structure_directory.join(structure_name);
+            let mut nbt_cell_file = std::fs::File::open(&nbt_cell_file).with_context(|| {
+                format!(
+                    "Failed to open structure file {:?} for reading",
+                    nbt_cell_file
+                )
+            })?;
+            let (cell, _): (Structure, _) = quartz_nbt::serde::deserialize_from(
+                &mut nbt_cell_file,
+                quartz_nbt::io::Flavor::GzCompressed,
+            )
+            .with_context(|| format!("Failed to parse structure for {:?}", structure_name))?;
+
+            let cell_extents = cell.blocks.iter().fold(
+                ((0, 0, 0), (0, 0, 0)),
+                |((lx, ly, lz), (mx, my, mz)), block| {
+                    (
+                        (
+                            std::cmp::min(lx, block.pos[0]),
+                            std::cmp::min(ly, block.pos[1]),
+                            std::cmp::min(lz, block.pos[2]),
+                        ),
+                        (
+                            std::cmp::max(mx, block.pos[0]),
+                            std::cmp::max(my, block.pos[1]),
+                            std::cmp::max(mz, block.pos[2]),
+                        ),
+                    )
+                },
+            );
+
+            let cell_data = PlacementStructureData {
+                sx: (((cell_extents.1).0) - ((cell_extents.0).0)) as u32 + 2 * XZ_EXPANSION,
+                sy: (((cell_extents.1).1) - ((cell_extents.0).1)) as u32,
+                sz: (((cell_extents.1).2) - ((cell_extents.0).2)) as u32 + 2 * XZ_EXPANSION,
+            };
+
+            Ok(self
+                .structure_cache
+                .entry(structure_name.to_owned())
+                .or_insert(cell_data))
+        }
+    }
+
+    pub fn build_cell(&mut self, cell: &Cell) -> Result<PlacementCell> {
+        match cell.r#type.as_ref() {
+            "MCPNR_SWITCHES" => self.build_switches(cell),
+            "MCPNR_LIGHTS" => self.build_lights(cell),
+            _ => self.build_from_nbt(cell),
+        }
+    }
+
+    pub fn build_switches<'design>(&mut self, cell: &Cell) -> Result<PlacementCell> {
+        let (x, y, z) = get_cell_pos(cell)?;
+        let nswitches = cell.get_param_i64("NSWITCH")?;
+        Ok(PlacementCell {
+            x,
+            y,
+            z,
+            sx: (nswitches as u32) * 2,
+            sy: 2,
+            sz: 3,
+            pos_locked: true,
+        })
+    }
+
+    pub fn build_lights<'design>(&mut self, cell: &Cell) -> Result<PlacementCell> {
+        let (x, y, z) = get_cell_pos(cell)?;
+        let nlight = cell.get_param_i64("NLIGHT")?;
+        Ok(PlacementCell {
+            x,
+            y,
+            z,
+            sx: (nlight as u32) * 2,
+            sy: 2,
+            sz: 2,
+            pos_locked: true,
+        })
+    }
+
+    pub fn build_from_nbt<'design>(&mut self, cell: &Cell) -> Result<PlacementCell> {
+        let sd = self.load_structure(&cell.r#type)?;
+
+        Ok(PlacementCell {
+            x: 0,
+            y: 0,
+            z: 0,
+            sx: sd.sx,
+            sy: sd.sy,
+            sz: sd.sz,
+            pos_locked: false,
+        })
+    }
+}
+
+fn get_cell_pos(cell: &Cell) -> Result<(u32, u32, u32)> {
+    Ok((
+        cell.get_param_i64_with_default("POS_X", 0)
+            .map_err(anyhow::Error::from)
+            .and_then(|v| v.try_into().map_err(anyhow::Error::from))
+            .with_context(|| "Read attr POS_X")?,
+        cell.get_param_i64_with_default("POS_Y", 0)
+            .map_err(anyhow::Error::from)
+            .and_then(|v| v.try_into().map_err(anyhow::Error::from))
+            .with_context(|| "Read attr POS_Y")?,
+        cell.get_param_i64_with_default("POS_Z", 0)
+            .map_err(anyhow::Error::from)
+            .and_then(|v| v.try_into().map_err(anyhow::Error::from))
+            .with_context(|| "Read attr POS_Z")?,
+    ))
+}
