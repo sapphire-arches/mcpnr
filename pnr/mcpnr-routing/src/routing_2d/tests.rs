246199fd08f97192673657ac6aebf61d851f842f
Reed Koser <srkoser+GitHub@gmail.com>
2021-12-14 13:45:03 -0800

    routing: make tests check routed path

--- pnr/mcpnr-routing/src/routing_2d/tests.rs
+++ pnr/mcpnr-routing/src/routing_2d/tests.rs
@@ -17,6 +17,11 @@ fn check_chain_for_error(
 fn it_can_route_straight_lines() -> Result<()> {
     let mut router = init(1, 3);
     router.route(Position::new(0, 0), Position::new(0, 2), RouteId(1))?;
+
+    assert_eq!(router.is_cell_occupied(Position::new(0, 0))?, RouteId(1));
+    assert_eq!(router.is_cell_occupied(Position::new(0, 1))?, RouteId(1));
+    assert_eq!(router.is_cell_occupied(Position::new(0, 2))?, RouteId(1));
+
     Ok(())
 }
 
@@ -45,6 +50,12 @@ fn crossing_similar_net_succeeds() -> Result<()> {
     let mut router = init(3, 3);
     router.route(Position::new(1, 0), Position::new(1, 2), RouteId(1))?;
     router.route(Position::new(0, 1), Position::new(2, 1), RouteId(1))?;
+
+    assert_eq!(router.is_cell_occupied(Position::new(1, 0))?, RouteId(1));
+    assert_eq!(router.is_cell_occupied(Position::new(1, 1))?, RouteId(1));
+    assert_eq!(router.is_cell_occupied(Position::new(1, 2))?, RouteId(1));
+    assert_eq!(router.is_cell_occupied(Position::new(0, 1))?, RouteId(1));
+    assert_eq!(router.is_cell_occupied(Position::new(2, 1))?, RouteId(1));
 
     Ok(())
 }
@@ -90,7 +101,52 @@ fn it_can_avoid_obstacles() -> Result<()> {
 
 #[test]
 fn it_can_choose_among_identical_paths() -> Result<()> {
-    let mut router = init(10, 10);
+    let mut router = init(3, 3);
 
-    router.route(Position::new(0, 0), Position::new(9, 9), RouteId(1))
+    router.route(Position::new(0, 0), Position::new(2, 2), RouteId(1))?;
+
+    // Detect which of the possible paths were taken
+    let c_00 = router.is_cell_occupied(Position::new(0, 0))?;
+    let c_01 = router.is_cell_occupied(Position::new(0, 1))?;
+    let c_02 = router.is_cell_occupied(Position::new(0, 2))?;
+    let c_10 = router.is_cell_occupied(Position::new(1, 0))?;
+    let c_11 = router.is_cell_occupied(Position::new(1, 1))?;
+    let c_12 = router.is_cell_occupied(Position::new(1, 2))?;
+    let c_20 = router.is_cell_occupied(Position::new(2, 0))?;
+    let c_21 = router.is_cell_occupied(Position::new(2, 1))?;
+    let c_22 = router.is_cell_occupied(Position::new(2, 2))?;
+
+    // Check endpoints
+    assert_eq!(c_00, RouteId(1));
+    assert_eq!(c_22, RouteId(1));
+
+    if c_02 == RouteId(1) {
+        // 1 1 1
+        // x x 1
+        // x x 1
+        assert_eq!(c_10, RouteId(1));
+        assert_eq!(c_21, RouteId(1));
+    } else if c_11 == RouteId(1) {
+        if c_10 == RouteId(1) {
+            // 1 1 x
+            // x 1 x
+            // x 1 1
+            assert_eq!(c_12, RouteId(1));
+        } else if c_01 == RouteId(1) {
+            // 1 x x
+            // 1 1 1
+            // x x 1
+            assert_eq!(c_21, RouteId(1));
+        }
+    } else if c_02 == RouteId(1) {
+        // 1 x x
+        // 1 x x
+        // 1 1 1
+        assert_eq!(c_01, RouteId(1));
+        assert_eq!(c_12, RouteId(1));
+    } else {
+        panic!("No valid route detected");
+    }
+
+    Ok(())
 }
