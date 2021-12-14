fcd2514ab713f46cb5d1b146ce31866ccbaec8e8
Reed Koser <srkoser+GitHub@gmail.com>
2021-12-14 15:21:51 -0800

    router: improve tests
      - rustfmt the file (oops)
      - print grid state to help debug
      - add ripup test

--- pnr/mcpnr-routing/src/routing_2d/tests.rs
+++ pnr/mcpnr-routing/src/routing_2d/tests.rs
@@ -13,14 +13,39 @@ fn check_chain_for_error(
     err.chain().find(predicate).is_some()
 }
 
+fn print_router_grid(router: &Router2D) -> Result<()> {
+    for y in 0..router.size_y {
+        for x in 0..router.size_x {
+            let pos = Position::new(x, y);
+            let idx = router.pos_to_idx(pos)?;
+
+            match router.grid[idx] {
+                GridCell::Occupied(RouteId(i)) => print!("{} ", i),
+                GridCell::Free => print!("x "),
+            }
+        }
+        println!()
+    }
+    Ok(())
+}
+
 #[test]
 fn it_can_route_straight_lines() -> Result<()> {
     let mut router = init(1, 3);
     router.route(Position::new(0, 0), Position::new(0, 2), RouteId(1))?;
 
-    assert_eq!(router.is_cell_occupied(Position::new(0, 0))?, RouteId(1));
-    assert_eq!(router.is_cell_occupied(Position::new(0, 1))?, RouteId(1));
-    assert_eq!(router.is_cell_occupied(Position::new(0, 2))?, RouteId(1));
+    assert_eq!(
+        router.is_cell_occupied(Position::new(0, 0))?,
+        Some(RouteId(1))
+    );
+    assert_eq!(
+        router.is_cell_occupied(Position::new(0, 1))?,
+        Some(RouteId(1))
+    );
+    assert_eq!(
+        router.is_cell_occupied(Position::new(0, 2))?,
+        Some(RouteId(1))
+    );
 
     Ok(())
 }
@@ -30,10 +55,12 @@ fn crossing_dissimilar_nets_fails() -> Result<()> {
     let mut router = init(3, 3);
 
     router.route(Position::new(1, 0), Position::new(1, 2), RouteId(1))?;
+    print_router_grid(&router)?;
 
-    let err = router
-        .route(Position::new(0, 1), Position::new(2, 1), RouteId(2))
-        .expect_err("Routing unexpectedly succeeded");
+    let err = router.route(Position::new(0, 1), Position::new(2, 1), RouteId(2));
+    print_router_grid(&router)?;
+
+    let err = err.expect_err("Routing unexpectedly succeeded");
 
     assert!(check_chain_for_error(err, |e| {
         match e.downcast_ref() {
@@ -51,11 +78,26 @@ fn crossing_similar_net_succeeds() -> Result<()> {
     router.route(Position::new(1, 0), Position::new(1, 2), RouteId(1))?;
     router.route(Position::new(0, 1), Position::new(2, 1), RouteId(1))?;
 
-    assert_eq!(router.is_cell_occupied(Position::new(1, 0))?, RouteId(1));
-    assert_eq!(router.is_cell_occupied(Position::new(1, 1))?, RouteId(1));
-    assert_eq!(router.is_cell_occupied(Position::new(1, 2))?, RouteId(1));
-    assert_eq!(router.is_cell_occupied(Position::new(0, 1))?, RouteId(1));
-    assert_eq!(router.is_cell_occupied(Position::new(2, 1))?, RouteId(1));
+    assert_eq!(
+        router.is_cell_occupied(Position::new(1, 0))?,
+        Some(RouteId(1))
+    );
+    assert_eq!(
+        router.is_cell_occupied(Position::new(1, 1))?,
+        Some(RouteId(1))
+    );
+    assert_eq!(
+        router.is_cell_occupied(Position::new(1, 2))?,
+        Some(RouteId(1))
+    );
+    assert_eq!(
+        router.is_cell_occupied(Position::new(0, 1))?,
+        Some(RouteId(1))
+    );
+    assert_eq!(
+        router.is_cell_occupied(Position::new(2, 1))?,
+        Some(RouteId(1))
+    );
 
     Ok(())
 }
@@ -69,10 +111,7 @@ fn routing_to_out_of_bounds_point_fails() -> Result<()> {
 
     assert!(check_chain_for_error(err, |e| {
         match e.downcast_ref() {
-            Some(RoutingError::OutOfBounds {
-                pos: Position { x: 0, y: 1 },
-                bounds: (1, 1),
-            }) => true,
+            Some(RoutingError::Unroutable) => true,
             _ => false,
         }
     }));
@@ -104,6 +143,7 @@ fn it_can_choose_among_identical_paths() -> Result<()> {
     let mut router = init(3, 3);
 
     router.route(Position::new(0, 0), Position::new(2, 2), RouteId(1))?;
+    print_router_grid(&router)?;
 
     // Detect which of the possible paths were taken
     let c_00 = router.is_cell_occupied(Position::new(0, 0))?;
@@ -117,36 +157,68 @@ fn it_can_choose_among_identical_paths() -> Result<()> {
     let c_22 = router.is_cell_occupied(Position::new(2, 2))?;
 
     // Check endpoints
-    assert_eq!(c_00, RouteId(1));
-    assert_eq!(c_22, RouteId(1));
+    assert_eq!(c_00, Some(RouteId(1)));
+    assert_eq!(c_22, Some(RouteId(1)));
 
-    if c_02 == RouteId(1) {
+    if let Some(c_20) = c_20 {
+        assert_eq!(c_20, RouteId(1));
         // 1 1 1
         // x x 1
         // x x 1
-        assert_eq!(c_10, RouteId(1));
-        assert_eq!(c_21, RouteId(1));
-    } else if c_11 == RouteId(1) {
-        if c_10 == RouteId(1) {
+        assert_eq!(c_10, Some(RouteId(1)));
+        assert_eq!(c_21, Some(RouteId(1)));
+    } else if let Some(c_11) = c_11 {
+        assert_eq!(c_11, RouteId(1));
+        if let Some(c_10) = c_10 {
+            assert_eq!(c_10, RouteId(1));
             // 1 1 x
             // x 1 x
             // x 1 1
-            assert_eq!(c_12, RouteId(1));
-        } else if c_01 == RouteId(1) {
+            assert_eq!(c_12, Some(RouteId(1)));
+        } else if let Some(c_01) = c_01 {
+            assert_eq!(c_01, RouteId(1));
             // 1 x x
             // 1 1 1
             // x x 1
-            assert_eq!(c_21, RouteId(1));
+            assert_eq!(c_21, Some(RouteId(1)));
         }
-    } else if c_02 == RouteId(1) {
+    } else if let Some(c_02) = c_02 {
+        assert_eq!(c_02, RouteId(1));
         // 1 x x
         // 1 x x
         // 1 1 1
-        assert_eq!(c_01, RouteId(1));
-        assert_eq!(c_12, RouteId(1));
+        assert_eq!(c_01, Some(RouteId(1)));
+        assert_eq!(c_12, Some(RouteId(1)));
     } else {
         panic!("No valid route detected");
     }
+
+    Ok(())
+}
+
+#[test]
+fn it_can_rip_up_tracks() -> Result<()> {
+    let mut router = init(1, 3);
+    router.route(Position::new(0, 0), Position::new(0, 2), RouteId(1))?;
+
+    assert_eq!(
+        router.is_cell_occupied(Position::new(0, 0))?,
+        Some(RouteId(1))
+    );
+    assert_eq!(
+        router.is_cell_occupied(Position::new(0, 1))?,
+        Some(RouteId(1))
+    );
+    assert_eq!(
+        router.is_cell_occupied(Position::new(0, 2))?,
+        Some(RouteId(1))
+    );
+
+    router.rip_up(RouteId(1))?;
+
+    assert_eq!(router.is_cell_occupied(Position::new(0, 0))?, None);
+    assert_eq!(router.is_cell_occupied(Position::new(0, 1))?, None);
+    assert_eq!(router.is_cell_occupied(Position::new(0, 2))?, None);
 
     Ok(())
 }
