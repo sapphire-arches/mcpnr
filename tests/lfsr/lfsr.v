a5b85b85213ea99467ca96d939829597bd709667
Reed Koser <srkoser+GitHub@gmail.com>
2023-11-24 15:28:16 -0800

    tests/lfsr: move lights and switches to encourage lower-block selection

--- tests/lfsr/lfsr.v
+++ tests/lfsr/lfsr.v
@@ -4,7 +4,7 @@ module top ();
 
   MCPNR_SWITCHES #(
     .POS_X(0),
-    .POS_Y(0),
+    .POS_Y(16),
     .POS_Z(0),
     .NSWITCH(2),
   ) input_switches (
@@ -16,7 +16,7 @@ module top ();
     for (i = 0; i < 64; i = i + 1) begin
       MCPNR_LIGHTS #(
         .POS_X(0 + (i * 2)),
-        .POS_Y(16),
+        .POS_Y(0),
         .POS_Z(0),
         .NLIGHT(1)
       ) output_lights (
